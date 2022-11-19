ef45bcb7f9946aab4c229171fb838f141e065cb2
tardis03 <becsdicoding@gmail.com>
2022-11-19 16:00:16 +0700

    belajar DOM

---
+++ script.js
@@ -0,0 +1,9 @@
+const email = document.querySelector("email");
+const password = document.querySelector("password");
+const tesBtn = document.querySelector("button");
+const con = document.querySelector(".login");
+
+tesBtn.onclick = () => {
+    alert(`sukses`);
+    con.innerHTML = "lg";
+}
