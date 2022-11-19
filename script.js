1f22d021e428a87fd70678639763af86e108599e
tardis03 <becsdicoding@gmail.com>
2022-11-19 19:19:59 +0700

    third

--- script.js
+++ script.js
@@ -1,100 +1,14 @@
 const email = document.querySelector("email");
 const password = document.querySelector("password");
 const tesBtn = document.querySelector("button");
-const con = document.querySelector(".login");
+const container = document.querySelector(".login");
 
 tesBtn.onclick = () => {
-    alert(`sukses`);
-    con.innerHTML = "lg";
+    alert(`meload login page`);
+    container.innerHTML = (`<label for="email">Email</label>
+    <input type="email" id="email" placeholder="masukkan email"></input>
+    <br>
+    <label for="password">Password</label>
+    <input type="password" id="password" placeholder="masukkan password"></input>
+    `);
 }
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
-console.log(`a`);
